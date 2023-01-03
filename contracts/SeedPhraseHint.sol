9a85b241801ac80edb4f69aaa94df451744a9f85
markcarey <mark@markcarey.com>
2023-01-03 10:55:49 -0500

    setup and initial contract code
    compiled but not deployed yet

---
+++ contracts/SeedPhraseHint.sol
@@ -0,0 +1,67 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract SeedPhraseHint is ERC721, AccessControl {
+    using Counters for Counters.Counter;
+    using SafeMath for uint256;
+
+    event SeedPhraseGuessed(address winner);
+
+    bytes32 public constant WINNER_ROLE = keccak256("WINNER_ROLE");
+    Counters.Counter private _tokenIdCounter;
+    uint256 mintPrice = 0.005 ether;
+
+    constructor() ERC721("Seed Phrase Hint", "HINT") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://api.seedphrase.pictures/meta/";
+    }
+
+    function safeMint(address to) public payable {
+        require(msg.value >= mintPrice, "!value");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function setMintPrice(uint256 _price) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        mintPrice = _price;
+    }
+
+    function exists(uint256 tokenId) external view returns (bool) {
+        return _exists(tokenId);
+    }
+
+    function withdraw() external onlyRole(WINNER_ROLE) {
+        // @dev: 5% to deployer
+        bool sent = payable(0xD167863Cb021A21B7aEb4d382CC64E038C2B90b2).send(address(this).balance.mul(5).div(100));
+        require(sent, "!send");
+        // @dev: 95% to winner
+        sent = payable(msg.sender).send(address(this).balance);
+        require(sent, "!send");
+        // @dev revoke WINNER role
+        _revokeRole(WINNER_ROLE, msg.sender);
+        emit SeedPhraseGuessed(msg.sender);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    receive() external payable {}
+}
---
+++ hardhat.config.js
@@ -0,0 +1,50 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+const dot = require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-waffle");
+const { API_URL_GOERLI, API_URL_ARBIGOERLI, PRIVATE_KEY, ETHERSCAN_API_KEY, ARBISCAN_API_KEY } = process.env;
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000
+          }
+        }
+      }
+    ] 
+},
+  defaultNetwork: "goerli",
+  networks: {
+    goerli: {
+      url: API_URL_GOERLI,
+      accounts: [`0x${PRIVATE_KEY}`],
+      gasMultiplier: 10,
+      gasPrice: 1000000000 * 10,
+      blockGasLimit: 0x1fffffffffffff
+    },
+    arbitrumgoerli: {
+      url: API_URL_ARBIGOERLI,
+      accounts: [`0x${PRIVATE_KEY}`],
+      gasMultiplier: 10,
+      gasPrice: 1000000000 * 10,
+      blockGasLimit: 0x1fffffffffffff
+    }
+  },
+   etherscan: {
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
+      arbitrumgoerli: ARBISCAN_API_KEY
+    }
+  }
+}
+
+// npx hardhat verify --network goerli 0x
